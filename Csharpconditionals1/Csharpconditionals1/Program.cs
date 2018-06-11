f0ee1cae275803f74d1d34838334c469c8b8ced0
jmockbee <mockbee1000@gmail.com>
2018-06-11 14:58:12 -0400

    some conditionals with breaks

---
+++ Csharpconditionals1/Csharpconditionals1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpconditionals1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.summer:
+                    Console.WriteLine(" the seasons are short ");
+                    break;
+
+                default:
+                    Console.WriteLine(" I don not understand what your taling about");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Csharpconditionals1/Csharpconditionals1/Season 2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpconditionals1
+{
+    public enum Season
+    {
+        spring,
+        summer,
+        Autumn,
+        Winter
+    }
+}
+
